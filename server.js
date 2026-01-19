7d81856c80cfcac5b726ae907fec206f7a9bd508
vnigoated <vninamdar03@gmail.com>
2026-01-19 15:10:32 +0530

    fix: Update allowed referrer URL and error message by removing the hash character.

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(bodyParser.json());
 
 // Middleware to restrict access to specific referrer
 app.use((req, res, next) => {
-    const allowedReferer = 'https://vois-cybercoach.vercel.app/#';
+    const allowedReferer = 'https://vois-cybercoach.vercel.app/';
     const referer = req.get('Referer');
     const host = req.get('host');
 
@@ -26,7 +26,7 @@ app.use((req, res, next) => {
     if ((referer && referer.startsWith(allowedReferer)) || (host && host.includes('localhost'))) {
         next();
     } else {
-        res.status(403).send('Access Denied: This lab can only be accessed via https://vois-cybercoach.vercel.app/#');
+        res.status(403).send('Access Denied: This lab can only be accessed via https://vois-cybercoach.vercel.app/');
     }
 });
 
